197ca4f77967533e58c433ac061e472c9c66bd2b
Edgar Villani Peres <edgarvperes@gmail.com>
2015-08-22 00:02:24 -0300

    Code refactor

--- app/src/main/java/com/seizonsenryaku/hayailauncher/StatusBarColorHelper.java
+++ app/src/main/java/com/seizonsenryaku/hayailauncher/StatusBarColorHelper.java
@@ -9,46 +9,32 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
-/**
- * Created by Edgar on 04-Aug-15.
- */
+
 public class StatusBarColorHelper {
-    public static void setStatusBarColor(Resources resources, Activity activity, int color) {
+    public static void setStatusBarColor(Resources resources, Activity activity) {
         //There's no support for colored status bar in versions below KITKAT
-        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) return;
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
 
-        final Window window = activity.getWindow();
-            //KITKAT path
-            //window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            final Window window = activity.getWindow();
 
 
-
-
-        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
-        //window.setStatusBarColor(Color.TRANSPARENT);
-        }
-
-
-        if(Build.VERSION.SDK_INT > 11) {
             window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
             window.getDecorView().setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                             | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+
+            int statusBarHeight = getStatusBarHeight(resources);
+
+            View statusBarDummy = activity.findViewById(R.id.statusBarDummyView);
+            statusBarDummy.getLayoutParams().height = statusBarHeight;
+
+            View topFillerView = activity.findViewById(R.id.topFillerView);
+            topFillerView.getLayoutParams().height = statusBarHeight;
+
+            View bottomFillerView = activity.findViewById(R.id.bottomFillerView);
+            bottomFillerView.getLayoutParams().height = statusBarHeight;
         }
-
-        int statusBarHeight = getStatusBarHeight(resources);
-
-        View statusBarDummy = activity.findViewById(R.id.statusBarDummyView);
-        statusBarDummy.getLayoutParams().height=statusBarHeight;
-
-        View topFillerView=activity.findViewById(R.id.topFillerView);
-        topFillerView.getLayoutParams().height =statusBarHeight;
-
-        View bottomFillerView=activity.findViewById(R.id.bottomFillerView);
-        bottomFillerView.getLayoutParams().height =statusBarHeight;
-
-
-
 
     }
 
