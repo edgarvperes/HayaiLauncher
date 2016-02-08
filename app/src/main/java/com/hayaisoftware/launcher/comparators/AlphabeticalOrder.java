a3450f59b094e1d9246372bb16b3d0ef2a77a7b2
seizonsenryaku <edgarvperes@gmail.com>
2016-02-08 16:27:37 -0200

    ignore case when doing alphabetical sorting (fixes #73)

--- app/src/main/java/com/hayaisoftware/launcher/comparators/AlphabeticalOrder.java
+++ app/src/main/java/com/hayaisoftware/launcher/comparators/AlphabeticalOrder.java
@@ -23,6 +23,6 @@ import java.util.Comparator;
 public class AlphabeticalOrder implements Comparator<LaunchableActivity> {
     @Override
     public int compare(LaunchableActivity lhs, LaunchableActivity rhs) {
-        return lhs.getActivityLabel().compareTo(rhs.getActivityLabel());
+        return lhs.getActivityLabel().compareToIgnoreCase(rhs.getActivityLabel());
     }
 }
